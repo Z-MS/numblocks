ecafb540143e91f677812362303ac5149ee69e09
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2022-06-15 17:47:32 +0100

    Set up ConfigCat client

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function init() {
+	let configCatClient = configcat.createClient("SDK-KEY");
+
+	configCatClient.getValueAsync("changeBlocksColor", false)
+	.then((value) => {
+		if(value) {
+			let colorBtn = document.querySelector('#colorBtn');
+			colorBtn.onclick = changeColor
+			colorBtn.style.display = 'block';
+		} else {
+			alert("Not enabled");
+		}
+	})
+
+	function changeColor() {
+		const colors = ['red', 'blue', 'yellow', 'golden', 'white', 'cyan', 'orange'];
+
+		// pick a random color from the array
+		let randColor = colors[Math.floor(Math.random() * (colors.length - 0) + 0)];
+		let firstBlockColor = document.querySelectorAll('.block')[0].style.backgroundColor;
+
+		while(randColor === firstBlockColor) {
+			randColor = colors[Math.floor(Math.random() * (colors.length - 0) + 0)];
+			if(firstBlockColor !== randColor) {
+				break;
+			}
+		}
+
+		const blocks = document.querySelectorAll('.block');
+
+		for(let i = 0; i < blocks.length; i++) {
+			blocks[i].style.backgroundColor = randColor;
+		}
+	}
+}
+
+init()
